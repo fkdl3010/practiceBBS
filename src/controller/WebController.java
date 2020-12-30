f228c2f1f7748aa0742079c8f68bdbda9c4cc736
fkdl3010 <fkdl3919@icloud.com>
2020-12-30 23:49:18 +0900

    add jar

---
+++ src/controller/WebController.java
@@ -0,0 +1,39 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("*.web")
+public class WebController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public WebController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html; charset=UTF-8");
+		
+		String requestURI = request.getRequestURI();
+		String context = request.getContextPath();
+		String cmd = requestURI.substring(context.length());
+		
+		
+		switch(cmd) {
+		
+		}
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
